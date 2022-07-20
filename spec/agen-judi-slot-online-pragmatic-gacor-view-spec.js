db5dcca6ee5de73de8e31d605957f056c52740d8
Mrcroot <50535472+reyspeed@users.noreply.github.com>
2022-07-21 01:40:32 +0700

    Update agen-judi-slot-online-pragmatic-gacor-view-spec.js

--- spec/agen-judi-slot-online-pragmatic-gacor-view-spec.js
+++ spec/agen-judi-slot-online-pragmatic-gacor-view-spec.js
@@ -1,6 +1,6 @@
 'use babel';
 
-import AgenjudislotonlinepragmaticgacorView from '../lib/Agen-judi-slot-online-pragmatic-gacor-view';
+import AgenjudislotonlinepragmaticgacorView from '../lib/agen-judi-slot-online-pragmatic-gacor-view';
 
 describe('AgenjudislotonlinepragmaticgacorView', () => {
   it('has one valid test', () => {
